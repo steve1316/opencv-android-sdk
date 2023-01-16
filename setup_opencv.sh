43c400d59d4c81e17ce47dcc7c04cf28f99f4023
Steve Tu <stevetu11@yahoo.com>
2023-01-15 22:52:02 -0800

    4.7.0 upload

---
+++ setup_opencv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+# Adjust opencv_version to designate which OpenCV version to download.
+opencv_version="4.7.0"
+file_name="opencv-${opencv_version}-android-sdk.zip"
+download_url="https://github.com/opencv/opencv/releases/download/${opencv_version}/opencv-${opencv_version}-android-sdk.zip"
+
+# Download the release zip file from its GitHub repo or skip the download if asked to.
+if [[ $1 = "--skip-download" ]]; then
+    echo "[INFO] Skipped download of ${file_name}"
+else
+    echo "[INFO] Preparing to download ${file_name} from its GitHub release link..."
+    echo "[INFO] Now downloading from: ${download_url}"
+    wget -c -O ${file_name} ${download_url}
+fi
+
+# Check if file finished downloading.
+if [[ ! -f ${file_name} ]]; then
+    echo "[ERROR] ${file_name} was not found!"
+    exit 1
+fi
+
+echo "[SUCCESS] Finished downloading OpenCV ${opencv_version} and verified its file location."
+
+# Now unzip the contents and move them to its required location.
+echo "[INFO] Extracting the OpenCV4Android.zip from the downloaded file into a temp/ folder."
+unzip -qqo ${file_name} -d temp/
+echo "[INFO] Unzipping the OpenCV4Android.zip file into the temp/opencv/ folder."
+unzip -qqo "temp/OpenCV4Android.zip" -d temp/opencv/
+
+echo "[INFO] Now copying sdk files from the temp/opencv/ folder to a opencv/ folder."
+cp -r temp/opencv/OpenCV-android-sdk/sdk/ opencv/
+
+# Finally, run its gradle and clean up the temp/ folder.
+echo "[INFO] Now running OpenCV's gradle..."
+echo "android { lintOptions { abortOnError false } }" >> opencv/build.gradle
+
+echo "[SUCCESS] OpenCV setup complete. Cleaning up now..."
+rm -rf temp
